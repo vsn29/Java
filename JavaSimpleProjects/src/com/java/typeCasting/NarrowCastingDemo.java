4ea283c69b5e57032f2ba6445e06d7eef4015784
vsn29 <vsnlearn29@gmail.com>
2020-10-08 00:22:27 +0530

    Type casting in java

---
+++ JavaSimpleProjects/src/com/java/typeCasting/NarrowCastingDemo.java
@@ -0,0 +1,22 @@
+package com.java.typeCasting;
+// Narrow casting is done by placing the type in parentheses in front of the value.
+
+public class NarrowCastingDemo {
+	
+	public static void main(String args[]){
+		
+		double typeDouble =29.2;
+		
+		//Converting larger type to smaller type
+		int typeInt = (int) typeDouble;
+		
+		System.out.println(typeDouble);
+		System.out.println(typeInt);
+
+	}
+	
+	
+	
+	
+
+}
